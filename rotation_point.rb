9626339f82fbb760df863967f0765f41e151bc9b
Mike Spangler <spangler.mike@gmail.com>
2015-07-15 21:33:59 -0700

    benchmark v parker.  he wins

--- rotation_point.rb
+++ rotation_point.rb
@@ -1,6 +1,15 @@
 require 'pry'
+require 'benchmark'
 
 WORDS = [
+  'p',
+  'pa',
+  'pb',
+  'pc',
+  'pd',
+  'pe',
+  'pf',
+  'pg',
   'ptolemaic',
   'retaliation',
   'retrograde',
@@ -21,6 +30,13 @@ WORDS = [
   'corporate',
   'engender',
   'karpatka',
+  'la',
+  'lb',
+  'lc',
+  'ld',
+  'le',
+  'lf',
+  'lg',
   'othellolagkage',
 ]
 
@@ -49,4 +65,36 @@ def walk(index)
   end
 end
 
-puts find_rotation_point
+puts "Me: #{Benchmark.measure { find_rotation_point }}"
+
+def find_rotation_pt
+
+  first_word = WORDS[0]
+
+  floor_index = 0
+  ceiling_index = WORDS.length - 1
+
+  while floor_index < ceiling_index 
+
+    # guess a point halfway between floor and ceiling
+    guess_index = floor_index + ((ceiling_index - floor_index) / 2)
+
+    # if guess comes before first word
+    if WORDS[guess_index] > first_word
+        # go right
+        floor_index = guess_index
+    else
+        # go left
+        ceiling_index = guess_index
+    end
+
+    # if floor and ceiling have converged
+    if floor_index + 1 == ceiling_index
+        # between floor and ceiling is where we flipped to the beginning
+        # so ceiling is alphabetically first
+        return WORDS[ceiling_index]
+    end
+  end
+end
+
+puts "Parker: #{Benchmark.measure { find_rotation_pt }}"
